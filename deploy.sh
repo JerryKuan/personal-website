da1f83f02fd3486205a6ff988668b8b11ed9502f
jiakuanfan <jiakuanfan@gmail.com>
2021-01-17 22:43:46 +0800

    update personal-website

--- deploy.sh
+++ deploy.sh
@@ -5,7 +5,7 @@ git fetch upstream
 git add .
 git commit -m 'update personal-website'
 git rebase upstream/master
-git push origin master
+git push -f origin master
 
 echo -e "\033[0;32mDeploying updates to GitHub...\033[0m"
 # Build the project.
@@ -21,7 +21,7 @@ if [ -n "$*" ]; then
 fi
 git commit -m "$msg"
 # Push source and build repos.
-git push origin master
+git push -f origin master
 # Come Back up to the Project Root
 cd ..
 echo -e "\033[0;32mFinished, enjoy your website!!!\033[0m"
